3bf63435952be7466ec57343cb38d5f3d6a84fd6
medeirosdev <guilherme.ggcm20@gmail.com>
2023-03-06 16:03:02 -0300

    all

---
+++ controllers/UserController.js
@@ -0,0 +1,152 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+// helpers
+//const getUserByToken = require('../helpers/get-user-by-token')
+//const getToken = require('../helpers/get-token')
+const createUserToken = require('../helpers/create-user-token')
+//const { imageUpload } = require('../helpers/image-upload')
+
+module.exports = class UserController {
+
+  static async register(req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const phone = req.body.phone
+    const password = req.body.password
+    const confirmpassword = req.body.confirmpassword
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: 'O nome é obrigatório!' })
+      return
+    }
+
+    if (!email) {
+      res.status(422).json({ message: 'O e-mail é obrigatório!' })
+      return
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: 'O telefone é obrigatório!' })
+      return
+    }
+
+    if (!password) {
+      res.status(422).json({ message: 'A senha é obrigatória!' })
+      return
+    }
+
+    if (!confirmpassword) {
+      res.status(422).json({ message: 'A confirmação de senha é obrigatória!' })
+      return
+    }
+
+    if (password != confirmpassword) {
+      res
+        .status(422)
+        .json({ message: 'A senha e a confirmação precisam ser iguais!' })
+      return
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+      res.status(422).json({ message: 'Por favor, utilize outro e-mail!' })
+      return
+    }
+
+    // create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    })
+
+    try {
+      const newUser = await user.save()
+
+      await createUserToken(newUser, req, res)
+      res.status(200).json({message: "deu bom" , newUser})
+
+    } catch (error) {
+      res.status(500).json({ message: error })
+    }
+  }
+
+
+  static async login(req, res) {
+    const email = req.body.email
+    const password = req.body.password
+
+    if (!email) {
+      res.status(422).json({ message: 'O e-mail é obrigatório!' })
+      return
+    }
+
+    if (!password) {
+      res.status(422).json({ message: 'A senha é obrigatória!' })
+      return
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+      return res
+        .status(422)
+        .json({ message: 'Não há usuário cadastrado com este e-mail!' })
+    }
+
+    // check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+      return res.status(422).json({ message: 'Senha inválida' })
+    }
+
+    await createUserToken(user, req, res)
+  }
+
+  static async checkUser(req, res) {
+    let currentUser
+
+    console.log(req.headers.authorization)
+
+    if (req.headers.authorization) {
+      const token = getToken(req)
+      const decoded = jwt.verify(token, 'nossosecret')
+
+      currentUser = await User.findById(decoded.id)
+
+      currentUser.password = undefined
+    } else {
+      currentUser = null
+    }
+
+    res.status(200).send(currentUser)
+  }
+
+  35:09 falta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ helpers/create-user-token.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const createUserToken = async (user, req, res) => {
+  const token = jwt.sign(
+    // payload data
+    {
+      name: user.name,
+      id: user._id,
+    },
+    "nossosecret" // para deixar o token único - usar strings únicas
+  );
+
+  // return token
+  res.status(200).json({
+    message: "Você está autenticado!",
+    token: token,
+    userId: user._id,
+  });
+};
+
+module.exports = createUserToken;
